b12f9a096d8f73e5ed8ce6a846c295b4421d84d8
Kiplangat Korir <korirkiplangat22@gmail.com>
2024-11-14 23:15:25 +0300

    New

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="graphfusion",
-    version="0.1.1",
+    version="0.1.2",
     description="GraphFusion Neural Memory Network SDK for adaptive knowledge management.",
     long_description=long_description,  
     long_description_content_type="text/markdown",  
