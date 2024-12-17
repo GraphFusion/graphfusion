d831e8fb09ea31375395edf7a4fc654f71e4c226
Kiplangat Korir <korirkiplangat22@gmail.com>
2024-12-17 18:34:28 +0300

    New

--- graphfusionai_project/tests/test_utilis.py
+++ graphfusionai_project/tests/test_utilis.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'graphfusionai')))
 import pytest
 import networkx as nx
-from graphfusionai_project.utils import Utils
+from graphfusionai.utils import Utils
 
 def test_validate_graph():
     g = nx.DiGraph()
