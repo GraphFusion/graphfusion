2cbb7c634b874a3c30656f99a879cb3a757013e0
Kiplangat Korir <korirkiplangat22@gmail.com>
2024-12-18 14:50:16 +0300

    New

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 
     keywords='AI, neural memory, knowledge graphs, deep learning',
     license='MIT',
